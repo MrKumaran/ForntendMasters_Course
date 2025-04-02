c58bd1746df40fe3a74f7ec09d264986d3ee3621
Kumaran S <kumaran.s@velammal.edu.in>
2025-04-03 00:47:15 +0530

    Tenth

--- app/src/main/java/com/example/frontendmasters/pages/menuPage.kt
+++ app/src/main/java/com/example/frontendmasters/pages/menuPage.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
